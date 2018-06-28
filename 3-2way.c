1dca1f5e0e3b9df5c448ef6412308eab60e681b4
gocjf0314 <gocjf12345@gmail.com>
2018-06-29 01:02:10 +0900

    B2-C1

---
+++ 3-2way.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main void(){
+        int a, b, d, e;
+        int c=a+b, f=d*e;
+        scanf("%d %d", &a, &b);
+        printf("%d+%d=%d", a, b, c);
+	pritnf("gocjf0314");
+        return 0;
+}
+
