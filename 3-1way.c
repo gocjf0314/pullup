ad2c8d23ab2aab9c3d78c3728bc3762ef24bb258
gocjf0314 <gocjf12345@gmail.com>
2018-06-29 00:33:47 +0900

    B1-C1

---
+++ 3-1way.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main void(){
+        int a, b, d, e, k, i=0;
+        int c=a+b, f=d*e;
+        scanf("%d %d", &a, &b);
+        printf("%d+%d=%d", a, b, c);
+	for(k=1; k<=10; k++){
+		i=i+k
+	}
+
+        return 0;
+}
+
